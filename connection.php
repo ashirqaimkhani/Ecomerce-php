67c14ba9544d66f742a88ac482121d8b15ed53d4
Ashar ahmed <wardaqaimkhani>
2024-06-03 03:38:20 +0500

    contact php work

---
+++ connection.php
@@ -0,0 +1,18 @@
+<?php 
+$servername = "localhost";
+$userrname = "root";
+$password = "";
+$dbname = "FA-PROJECT";
+$conn = mysqli_connect($servername,$userrname,$password,$dbname);
+// if($conn){
+//     echo "connection";
+// }else{
+//     echo "not connection";
+// }
+
+
+
+
+
+
+?>
---
+++ mysql/fa-project.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 03, 2024 at 12:28 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `fa-project`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact`
+--
+
+CREATE TABLE `contact` (
+  `id` int(11) NOT NULL,
+  `Name` varchar(20) NOT NULL,
+  `Email` varchar(50) NOT NULL,
+  `subject` varchar(50) NOT NULL,
+  `Decription` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `contact`
+--
+
+INSERT INTO `contact` (`id`, `Name`, `Email`, `subject`, `Decription`) VALUES
+(1, 'Ashar Ahmed', '', 'php', 'ashir naveed ahmed khan qaim khani'),
+(2, 'Ashar Ahmed', 'AHIR@GMAIL.COM', 'php', 'ASHI@ma');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `contact`
+--
+ALTER TABLE `contact`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `contact`
+--
+ALTER TABLE `contact`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
